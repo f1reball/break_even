05a330702c6a2c81e96d0e7b4e911be01c86b8ec
Jackson Booth <jacksontbooth@icloud.com>
2022-02-17 22:37:37 +1100

    added useEffect to make updating easier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Input from './components/input.js';
 
 function App() {
 
-  const [data, setData] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [quantity, setQuantity] = useState(null);
+  const [calc, setCalc] = useState(null);
+
+
+  useEffect(() => {
+    setCalc(price * quantity);
+  }, [price, quantity])
+
 
   return (
     <div className="App">
-      <h1>{data}</h1>
-      <Input stateController={setData} />
+
+      <h1>{price}</h1>
+      <Input stateController={setPrice} />
+      <h1>{quantity}</h1>
+      <Input stateController={setQuantity} />
+
+      <h1>hello {calc}</h1>
     </div>
   );
 }
